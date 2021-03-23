a701fdd433177cd793b40581ef88335b2356f37f
Jonathan Smith <jpls218@gmail.com>
2021-03-23 09:57:11 -0400

    more format changes for portfolio page

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -5,6 +5,7 @@ import Row from "../components/Row";
 import Col from "../components/Col";
 import "./style.css";
 
+
 function Portfolio() {
   return (
     <div>
@@ -17,42 +18,42 @@ function Portfolio() {
         <Row>
           <Col size="md-4">
           <u>Weather Dashboard</u>
-          <img className="img-thumbnail" src="https://raw.githubusercontent.com/jpls218/Portfolio/main/public/images/Weather_Dashboard.jpeg" width="350" height="350" alt="weather" />
-          <a href="https://github.com/jpls218/WeatherReport.git" target="_blank" rel="noreferrer"><button type="button" className="btn btn-primary">Github</button></a>
-          <a href="https://jpls218.github.io/WeatherReport/" target="_blank" rel="noreferrer"><button type="button" className="btn btn-primary">Link</button></a>
+          <img id="pic" className="img-thumbnail" src="https://raw.githubusercontent.com/jpls218/Portfolio/main/public/images/Weather_Dashboard.jpeg" width="350" height="350" alt="weather" />
+          <a id="links" href="https://github.com/jpls218/WeatherReport.git" target="_blank" rel="noreferrer"><button type="button" className="btn btn-primary">Github</button></a>
+          <a id="links" href="https://jpls218.github.io/WeatherReport/" target="_blank" rel="noreferrer"><button type="button" className="btn btn-primary">Link</button></a>
           </Col>
           <Col size="md-4">
           <u>battleNodes (group project!)</u>
-          <img className="img-thumbnail" src="https://raw.githubusercontent.com/jpls218/Portfolio/main/public/images/battleNodes.jpeg" width="350" height="350" alt="battlenodes" />
-          <a href="https://github.com/CastroAlejandro/whos-your-data.git" target="_blank" rel="noreferrer"><button type="button" className="btn btn-primary">Github</button></a>
-          <a href="https://protected-beach-35913.herokuapp.com/" target="_blank" rel="noreferrer"><button type="button" className="btn btn-primary">Link</button></a>
+          <img id="pic" className="img-thumbnail" src="https://raw.githubusercontent.com/jpls218/Portfolio/main/public/images/battleNodes.jpeg" width="350" height="350" alt="battlenodes" />
+          <a id="links" href="https://github.com/CastroAlejandro/whos-your-data.git" target="_blank" rel="noreferrer"><button type="button" className="btn btn-primary">Github</button></a>
+          <a id="links" href="https://protected-beach-35913.herokuapp.com/" target="_blank" rel="noreferrer"><button type="button" className="btn btn-primary">Link</button></a>
           </Col>
           <Col size="md-4">
           <u>Workday Schedule</u>
-          <img className="img-thumbnail" src="https://raw.githubusercontent.com/jpls218/Portfolio/main/public/images/Workday_Schedule.jpeg" width="350" height="350" alt="weather" />
-          <a href="https://github.com/jpls218/WorkDaySchedule.git" target="_blank" rel="noreferrer"><button type="button" className="btn btn-primary">Github</button></a>
-          <a href="https://jpls218.github.io/WorkDaySchedule/" target="_blank" rel="noreferrer"><button type="button" className="btn btn-primary">Link</button></a>
+          <img id="pic" className="img-thumbnail" src="https://raw.githubusercontent.com/jpls218/Portfolio/main/public/images/Workday_Schedule.jpeg" width="350" height="350" alt="weather" />
+          <a id="links" href="https://github.com/jpls218/WorkDaySchedule.git" target="_blank" rel="noreferrer"><button type="button" className="btn btn-primary">Github</button></a>
+          <a id="links" href="https://jpls218.github.io/WorkDaySchedule/" target="_blank" rel="noreferrer"><button type="button" className="btn btn-primary">Link</button></a>
           </Col>
         </Row>
         <Row>
-        <Col size="md-4">
-        <u>Eat-Da-Burger!</u>
-        <img className="img-thumbnail" src="https://raw.githubusercontent.com/jpls218/Portfolio/main/public/images/Eat-Da-Burger.jpeg" width="350" height="350" alt="weather" />
-        <a href="https://github.com/jpls218/Burgers_Node_Express.git" target="_blank" rel="noreferrer"><button type="button" className="btn btn-primary">Github</button></a>
-        <a href="https://jpls218.github.io/Burgers_Node_Express/" target="_blank" rel="noreferrer"><button type="button" className="btn btn-primary">Link</button></a>
-        </Col>
-        <Col size="md-4">
-        <u>Password Generator</u>
-        <img className="img-thumbnail" src="https://raw.githubusercontent.com/jpls218/Portfolio/main/public/images/Password_Generator.jpeg" width="350" height="350" alt="weather" />
-        <a href="https://github.com/jpls218/Password_Generator.git" target="_blank" rel="noreferrer"><button type="button" className="btn btn-primary">Github</button></a>
-        <a href="https://jpls218.github.io/Password_Generator/" target="_blank" rel="noreferrer"><button type="button" className="btn btn-primary">Link</button></a>
-        </Col>
-        <Col size="md-4">
-        <u>Employee Directory</u>
-        <img className="img-thumbnail" src="https://raw.githubusercontent.com/jpls218/Portfolio/main/public/images/Employee_Directory.jpeg" width="350" height="350" alt="weather" />
-        <a href="https://github.com/jpls218/React_Employee_Directory.git" target="_blank" rel="noreferrer"><button type="button" className="btn btn-primary">Github</button></a>
-        <a href="https://jpls218.github.io/React_Employee_Directory/" target="_blank" rel="noreferrer"><button type="button" className="btn btn-primary">Link</button></a>
-        </Col>
+          <Col size="md-4">
+          <u>Eat-Da-Burger!</u>
+          <img id="pic" className="img-thumbnail" src="https://raw.githubusercontent.com/jpls218/Portfolio/main/public/images/Eat-Da-Burger.jpeg" width="350" height="350" alt="weather" />
+          <a id="links" href="https://github.com/jpls218/Burgers_Node_Express.git" target="_blank" rel="noreferrer"><button type="button" className="btn btn-primary">Github</button></a>
+          <a id="links" href="https://jpls218.github.io/Burgers_Node_Express/" target="_blank" rel="noreferrer"><button type="button" className="btn btn-primary">Link</button></a>
+          </Col>
+          <Col size="md-4">
+          <u>Password Generator</u>
+          <img id="pic" className="img-thumbnail" src="https://raw.githubusercontent.com/jpls218/Portfolio/main/public/images/Password_Generator.jpeg" width="350" height="350" alt="weather" />
+          <a id="links" href="https://github.com/jpls218/Password_Generator.git" target="_blank" rel="noreferrer"><button type="button" className="btn btn-primary">Github</button></a>
+          <a id="links" href="https://jpls218.github.io/Password_Generator/" target="_blank" rel="noreferrer"><button type="button" className="btn btn-primary">Link</button></a>
+          </Col>
+          <Col size="md-4">
+          <u>Employee Directory</u>
+          <img id="pic" className="img-thumbnail" src="https://raw.githubusercontent.com/jpls218/Portfolio/main/public/images/Employee_Directory.jpeg" width="350" height="350" alt="weather" />
+          <a id="links" href="https://github.com/jpls218/React_Employee_Directory.git" target="_blank" rel="noreferrer"><button type="button" className="btn btn-primary">Github</button></a>
+          <a id="links" href="https://jpls218.github.io/React_Employee_Directory/" target="_blank" rel="noreferrer"><button type="button" className="btn btn-primary">Link</button></a>
+          </Col>
         </Row>
       </Container>
     </div>
